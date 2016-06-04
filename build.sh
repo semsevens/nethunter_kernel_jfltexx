909ae507b0a148e1fefc5a9650ab141ac3656ed4
semsevens <semsevens@gmail.com>
2016-06-05 03:18:09 +0800

    change the output dir to build
    Change-Id: I1e87209ba63207d03d9062b753a5aa935922407b

--- build.sh
+++ build.sh
@@ -3,10 +3,16 @@
 
 RDIR=$(pwd)
 THREADS=5
+DEFCONFIG=kali_defconfig
+DEVICE=jfltexx
 
 export ARCH=arm
 export CROSS_COMPILE=`pwd`/toolchain/bin/arm-cortex_a15-linux-gnueabihf-
 
-make clean
-make kali_defconfig
-make -j"$THREADS"
+echo "Cleaning build..."
+rm -rf build && mkdir build
+
+make -C $RDIR O=build $DEFCONFIG &&
+echo "Starting build for $DEVICE..." &&
+make -C $RDIR O=build -j"$THREADS" &&
+echo "Done building $DEVICE."
